9b0781e8691fb2e99725b4cd9b53e73c3d5b0e2c
TemaMo <artyambs@mail.ru>
2023-04-09 10:03:41 +0300

    initial commit

--- myproject/settings.py
+++ myproject/settings.py
@@ -4,21 +4,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-okft1w^@x(pjs)2gwt4k6)u3n-h^g&h3idtt9m_a4(jyh3okq9"
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
-
-
-# Application definition
-
 INSTALLED_APPS = [
     "django.contrib.admin",
     "django.contrib.auth",
